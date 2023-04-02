741afea149ab37f302b63680d2c53d52b76af140
pierre <dommerc.pierre@gmail.com>
2023-04-02 21:07:48 +0200

    deip keymap wip

--- keyboards/dztech/tofu/jr/keymaps/deip/keymap.c
+++ keyboards/dztech/tofu/jr/keymaps/deip/keymap.c
@@ -28,8 +28,8 @@
 
 enum layers {
     _BL = 0, // base
-    _FL,     // F* keys
-    _ML,     // macros
+    _FL,     // F* keys & some special keys
+    _EL,     // not used for now
 };
 
 enum keycodes {
@@ -46,20 +46,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    FR_ASTR, KC_EQL,  KC_BSPC, KC_TILDE,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO(_FL),
-        OSL(_ML), KC_LGUI, KC_LALT,                            KC_SPC,           KC_RALT, KC_RGUI, KC_CAPS,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   OSL(_FL),
+        OSL(_FL), KC_LGUI, KC_LALT,                          KC_SPC,             KC_RALT, KC_RGUI, KC_CAPS,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FL] = LAYOUT_65_ansi(
         QK_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_GRV,
-        RGB_RES, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT, KC_HOME,
-        _______, RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EE_CLR,  KC_END,
+        RGB_RES, FR_AGRV, _______, FR_ECIR, _______, _______, _______, FR_UGRV, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT, KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EE_CLR,  KC_END,
         KC_LSFT, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______,          KC_VOLU, KC_MUTE,
         KC_CAPS, _______, _______,                            _______,          _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT
     ),
-    [_ML] = LAYOUT_65_ansi(
+    [_EL] = LAYOUT_65_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, FR_AGRV, _______, FR_ECIR, _______, _______, _______, FR_UGRV, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,    LARW, _______, _______,    RARW,   RARWD,          _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______,                            _______,          _______, _______, _______,          _______, _______, _______
     )
@@ -121,13 +121,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // keep us qwerty key ]
             return key_override_reg(FR_RBRC, MOD_MASK_SHIFT | MOD_MASK_ALT, record);
         case KC_QUOT:
-            if (IS_LAYER_ON(_ML)) {
+            if (IS_LAYER_ON(_FL)) {
                 return true;
             }
             // convert " to $
             return key_override_reg(FR_DLR, MOD_MASK_SHIFT, record);
         case KC_0:
-            if (IS_LAYER_ON(_ML)) {
+            if (IS_LAYER_ON(_FL)) {
                 return true;
             }
             // convert 0 to )
